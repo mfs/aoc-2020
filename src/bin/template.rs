a678a4d1ac70f672f1c73ce8a1c58f6e655e2805
Mike Sampson <mike@sambodata.com>
2020-11-27 01:15:33 +1100

    Add some boilerplate

--- src/bin/template.rs
+++ src/bin/template.rs
@@ -3,5 +3,30 @@ use anyhow::Result;
 fn main() -> Result<()> {
     println!("Advent of Code 2020!");
 
+    /* process stdin as lines
+    use std::io::{self, BufRead};
+    for line in io::stdin().lock().lines() {
+        println!("{}", line?);
+    }
+    */
+
+    /* process stdin as chars (ascii only)
+    use std::io::{self, Read};
+    for byte in io::stdin().lock().bytes() {
+        println!("{}", byte? as char);
+    }
+    */
+
+    /* process stdin as lines of space separated ints
+    use std::io::{self, BufRead};
+    for line in io::stdin().lock().lines() {
+        let x: Vec<u64> = line?
+            .split(' ')
+            .map(|x| x.parse())
+            .collect::<Result<_, _>>()?;
+        println!("{:?}", x);
+    }
+    */
+
     Ok(())
 }
