a8d1ed13594b9322e3f2f65b218599ffd410eaa7
Mike Sampson <mike@sambodata.com>
2020-12-01 23:20:39 +1100

    Make loops more efficient, don't worry about early out

--- src/bin/p01.rs
+++ src/bin/p01.rs
@@ -27,12 +27,11 @@ fn p1(numbers: &Vec<u64>) {
 fn p2(numbers: &Vec<u64>) {
     let len = numbers.len();
 
-    'outer: for a in 0..len {
-        for b in 0..len {
-            for c in 0..len {
+    for a in 0..len {
+        for b in a+1..len {
+            for c in b+1..len {
                 if numbers[a] + numbers[b] + numbers[c] == 2020 {
                     println!("Part 2 = {}", numbers[a] * numbers[b] * numbers[c]);
-                    break 'outer;
                 }
             }
         }
