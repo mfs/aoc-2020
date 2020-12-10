999718250c78b0e823e0d801142ffe0c54cab026
Mike Sampson <mike@sambodata.com>
2020-12-10 23:58:57 +1100

    Puzzle 10
    I could see a recursive solution to part 2 however the puzzle text
    hinted that this wouldn't be feasible. Spent a lot of time looking
    at my diff Vec trying to see a shortcut. Eventually came up with
    this solution after playing around with pen and paper. Part two of
    today is easily the toughest so far for me.

---
+++ src/bin/p10.rs
@@ -0,0 +1,51 @@
+use anyhow::{anyhow, Result};
+use std::io::{self, BufRead};
+use std::collections::HashMap;
+
+fn main() -> Result<()> {
+    let mut adapters: Vec<i64> = vec![];
+
+    for line in io::stdin().lock().lines() {
+        adapters.push(line?.parse()?);
+    }
+
+    adapters.push(0);
+    adapters.sort();
+    let last = adapters.last().ok_or(anyhow!("empty input"))? + 3;
+    adapters.push(last);
+
+    println!("Part 1 = {}", p1(&adapters));
+
+    println!("Part 2 = {}", p2(&adapters)[&last]);
+
+    Ok(())
+}
+
+fn p1(adapters: &Vec<i64>) -> usize {
+    let mut diff = vec![];
+
+    for a in adapters.iter().zip(adapters[1..].iter()) {
+        diff.push(a.1 - a.0);
+    }
+
+    let ones = diff.iter().filter(|&x| x == &1).count();
+    let threes = diff.iter().filter(|&x| x == &3).count();
+
+    ones * threes
+}
+
+fn p2(adapters: &Vec<i64>) -> HashMap<i64, i64> {
+    let mut paths = HashMap::new();
+    paths.insert(0, 1);
+
+    for n in adapters {
+        for i in 1..4 {
+            if adapters.contains(&(n + i)) {
+                *paths.entry(n+i).or_insert(0) += paths[n];
+            }
+        }
+    }
+
+    paths
+}
+
